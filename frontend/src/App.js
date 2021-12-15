377aa392fd7eea75fcb1a04ccdcf8af81ca798e2
akshita007 <69304703+akshita007@users.noreply.github.com>
2021-12-15 16:43:22 +0530

    css and git ignore updated, selling form route add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,32 @@ import Navbartask from "./components/Navbar.js";
 import Footer from "./components/footer.js";
 import Home from './pages/Home';
 import LandingPage from "./pages/landingPage";
+import Register from "./pages/Register";
+import {useEffect} from "react";
+import {logoutUser, verifyToken} from "./services/api";
+import SellingForm from "./pages/SellForm";
 
 function App() {
+  useEffect(()=>{
+    const checkToken = async()=>{
+      if(localStorage.getItem("token")!==null){
+        let data = await verifyToken();
+        if(!data.success){
+          await logoutUser();
+          window.location.reload();
+        }
+      }
+    }
+    checkToken()
+  },[])
   return (
   <Router>
     <Navbartask/>
       <Routes>
         <Route exact path="/" element = {<LandingPage/>}></Route>
         <Route path="/homepage" element = {<Home/>}></Route>
+        <Route path="/register" element = {<Register/>}></Route>
+        <Route path="/sellForm" element = {<SellingForm/>}></Route>
       </Routes>
     <Footer/>
   </Router>
