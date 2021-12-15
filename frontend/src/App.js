950b9606a544da658ba40f5ba4e24193921fa81d
riyarajj <66518009+riyarajj@users.noreply.github.com>
2021-12-15 10:57:14 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Navbartask from "./components/Navbar.js";
 import Footer from "./components/footer.js";
 import Home from './pages/Home';
+
+import { Landing } from './Landing';
 import LandingPage from "./pages/landingPage";
 
 function App() {
@@ -10,7 +12,8 @@ function App() {
   <Router>
     <Navbartask/>
       <Routes>
-        <Route exact path="/" element = {<LandingPage/>}></Route>
+        <Route exact path="/" element = {<Landing/>}></Route>
+        <Route exact path="/LandingPage" element = {<LandingPage/>}></Route>
         <Route path="/homepage" element = {<Home/>}></Route>
       </Routes>
     <Footer/>
