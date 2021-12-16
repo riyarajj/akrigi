85376b25df8ac4085718d14239cc33727b475819
akshita007 <69304703+akshita007@users.noreply.github.com>
2021-12-16 20:38:08 +0530

    passprt initialised

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
+var passport = require("passport");
 
 const mongoose = require("mongoose");
 const dotenv = require('dotenv');
@@ -45,6 +46,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
+app.use(passport.initialize())
 
 app.use('/', indexRouter);
 app.use('/api/auth',authRouter);
