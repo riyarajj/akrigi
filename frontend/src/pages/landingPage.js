ebd53e7d631a84e1cc37036058b2cd4ea4817a61
akshita007 <69304703+akshita007@users.noreply.github.com>
2021-12-16 20:36:19 +0530

    css improved

--- frontend/src/pages/landingPage.js
+++ frontend/src/pages/landingPage.js
@@ -7,18 +7,18 @@ const LandingPage = ()=>{
           <Link to = "/sellForm" className = "links">
             <div className = "box">
               <img src = "/images/boxImageSell.jpg" alt = "Sell" className = "boxImage"/>
-                <span>Sell</span>
+                <span className="boxText">Sell</span>
             </div>
           </Link>
           <Link to = "/homepage" className = "links">
             <div className = "box">
               <img src="/images/boxImagePurchase.jpg" alt = "Purchase" className = "boxImage"/>
-              <span>Purchase</span>
+              <span className="boxText">Purchase</span>
             </div>
           </Link>
             <div className = "box">
               <img src="/images/boxImageDonate.jpg" alt = "Donate" className = "boxImage"/>
-              <span>Donate</span>
+              <span className="boxText">Donate</span>
             </div>
         </div>
         
