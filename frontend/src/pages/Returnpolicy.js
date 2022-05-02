264fee031be2782445311310003a208f21fbcd9c
riyarajj <66518009+riyarajj@users.noreply.github.com>
2022-05-02 16:51:47 +0530

    Add files via upload

---
+++ frontend/src/pages/Returnpolicy.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import { Container, Row } from 'react-bootstrap'
+import {Link} from "react-router-dom"
+
+const Returnpolicy = () => {
+  return (
+    <div>
+        <Container>
+            <center><h1 className='my-5'>Cancellation, Exchange & Returns</h1></center>
+            <div  className=' px-5 justifycontent textreponsive'>
+                
+                    <p>Cancellation until item shipment: You may cancel an order until the item has been shipped. To cancel your order click on the 'Contact Support' or 'Help' link on our website. Once you arrive at the page, choose the issue 'Cancellation & Refund' and fill in your reasons for your order cancellation. Thereafter, you shall get an email confirmation from our side of your order cancellation. If you have already made the payment, you shall receive your Refund within 10 working days of receiving the confirmation of cancellation from our side.</p>
+                    <p>Cancellation & Refunds after the item has been shipped: Once an order has been shipped, cancellation will not be proceeded.</p>
+                    <div>
+                        <div>
+                            <h5>Cancellation and Refund Policy:</h5>
+                            <ol>
+                                <li>You cannot cancel the order once the order has been shipped. </li>
+                                <li>You may cancel an order until the item has been shipped.. You shall receive an email from our side either confirming or declining your request based on the reasons provided by you. The decision of Akrigi regarding the same would be final and non-negotiable.</li>
+                                <li>If the item is already paid for by using our Payment gateway, the refund would be either processed in the same account from where the payment to us was originally made or we will request for your Bank details to process your refund or your GPay Number.</li>
+                            </ol>
+                        </div>
+                        <div>
+                            <h5>How does Cancellation policy work?</h5>
+                            <ul>
+                                <li>Once the shipment has been dispatched, we DO NOT accept cancellations.</li>
+                                <li>The shipment will attempt delivery TWICE – That’s a fair deal! Should you refuse to accept delivery, remain unavailable/ unreachable during all two attempts or cancel the shipment with us, we will consider you to be a DEFAULTER.</li>
+                                <li>A DEFAULTER cannot shop with us on the “cash on delivery” option in the future. Incase you prepaid on your cancelled order, we will refund 50% of the shipment value to you excluding delivery charges.</li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h5>Returns:</h5>
+                            <ul>
+                                <li>We DO NOT accept returns.</li>
+                                <b>WHY?</b>
+                                <li>The cost of free returns: Akrigi loses a third of their revenue to returns and thus donations to the charity organizations decreases.</li>
+                                <li>Environmental Costs: As returns are not affordable to masses and cause losses to the business they are often end up in landfill resulting in carbon emissions. The returns culture and consumer behaviour of ‘buying more to return’ will have a significant and unnecessary carbon footprint.</li>
+                                <li>Each returned package, regardless of which carrier picks it up leaves a trail of emissions from the various trains, planes, and giant trucks that carry it back to the seller. That pollution contributes to climate change and worsens air quality.</li>
+                                <li>The amount of single-use packaging is then doubled if the customer exchanges an item.</li>
+                                <li>Looking at the above pointers, if we allow free returns, the whole idea behind thrift shopping doesn’t make any sense.</li>
+                            </ul>
+                        </div>
+                        <div className='my-3'>
+                            <b><h4>EXCHANGE</h4></b>
+                            <p>
+                            If there is any issue with the product (wrong/defective piece), you can raise an Exchange request within 3 days of receiving the order.
+                            <h6>Process:</h6>
+                            Connect <Link to='/contactus'>here</Link> with Subject as Exchange Request and Context with order details, your query, and an image of the wrong/defective product within 3 days of product delivery. 
+                            </p>
+                            <p>(Note: Exchange Policy is only available for items from the 'Surplus' and 'Fashion Accessories' section. Exchange is not applicable for thrift items.)</p>
+                        </div>
+                        <div className='my-3'>
+                            <b><h4>SHIPPING</h4></b>
+                            <p>We strive to deliver products purchased from Akrigi in excellent condition and in the fastest time possible. For all the Orders of Rs. 2999/- or more, we will deliver the order to your doorstep free of cost. A platform handling fee of Rs. 99/- will be applicable to the orders under Rs. 2999/-.
+                                <ol>
+                                    <li><p> If the order is cancelled, lost or un - delivered to your preferred location, we will refund the complete order amount including any platform handling fee, if paid online.</p></li>
+                                    <li><p>For accounts whose return behavior violates our fair usage policy, platform handling fee will be non - refundable irrespective of order value.</p></li>
+                                </ol>
+                            </p>
+                        </div>
+
+                    </div>
+                
+            </div>
+        </Container>
+    </div>
+  )
+}
+
+export default Returnpolicy
---
+++ frontend/src/pages/TC.js
@@ -0,0 +1,93 @@
+import React from 'react'
+import { Container, Row } from 'react-bootstrap'
+import {Link} from "react-router-dom"
+
+const TC = () => {
+  return (
+    <div>
+      <Container>
+        <center><h1 className='my-5'>Terms and Condition</h1></center>
+        <Row  className='px-5 justifycontent textreponsive'>
+            <p >
+                Dear Users, please read and accept the following terms and conditions very carefully before your use of our website www.akrigi.com. Your use of www.akrigi.com is a confirmation of your acceptance and compliance with the following terms and conditions (henceforth referred to as "Terms").</p>
+
+            <p>We reiterate that by subscribing to or using any of our services you agree that you have read, understood and are bound by the Terms, regardless of how you subscribe to or use the services. If you do not want to be bound by the Terms, you must not subscribe to or use our services. These Terms and various other policies are binding as per the provisions of the Information Technology Rules, 2011 formulated under the Information Technology Act of 2000.</p>
+
+            <p>In these Terms, references to "you", "user" shall mean the end user accessing the Website, its contents and using the Services offered through the Website, "Service Providers" “Logistics Partner” mean independent third party service providers, and "we", "us",“our”, “akrigi”, and  “akrigi.com” shall mean our Company  and its affiliates.</p>
+
+            <p>The Company may amend the Terms by posting a revised version on the website from time to time. The revised version would be effective from the time we post it on the Site and in the event you continue to use our website you are impliedly agreeing to the revised Terms.</p>
+            <div className='my-2'>
+              <ol>
+                <li>
+                  <b>General Overview</b>
+                  <ol type='a'>
+                    <li><p>www.akrigi.com or akrigi ("Website") is an Internet based content and e-commerce (Online retail) portal. akrigi is an online thrift fashion community that provides users with a platform to buy new or pre-owned designer /non-designer merchandise.</p></li>
+                    <li><p>Use of the Website is offered to you on the condition of your acceptance without modification of all the terms, conditions and notices contained in these Terms, as may be posted on the Website from time to time. akrigi at its sole discretion reserves the right not to accept a user from registering on the Website without assigning any reason thereof.</p></li>
+                    <li><p>The right to use the Site is personal to the User and is not transferable to any other person or entity. The User shall be responsible for protecting the confidentiality of their password(s), if any. The User acknowledges that, although the Internet is often a secure environment, sometimes there are interruptions in service or events that are beyond the control of the Company, and the Company shall not be responsible for any data lost while transmitting information on the Internet. While it is the Company’s objective to make the Site accessible 24 hours a day, the Site may be unavailable from time to time for any reason including, without limitation, routine maintenance. You understand and acknowledge that due to circumstances both within and outside of the control of the Company, access to the Site may be interrupted, suspended or terminated from time to time. The Company shall have the right at any time to change or discontinue any aspect or feature of the Site, including, but not limited to, content, hours of availability and equipment needed for access or use. Further, the Company may discontinue disseminating any portion of information or category of information may change or eliminate any transmission method and may change transmission speeds or other signal characteristics.</p></li>
+                  </ol>
+                </li>
+                <li>
+                  <b>User Eligibility and Website Access</b>
+                  <ol type='a'>
+                  <li><p>Use of the Site is available only to persons who can form legally binding contracts under Indian Contract Act, 1872. Persons who are "incompetent to contract" within the meaning of the Indian Contract Act, 1872 including minors, un-discharged insolvents, of unsound mind are not eligible to use the Site. If you are a minor i.e. under the age of 18 years, you cannot register as a member of the Site and shall not purchase any items on the Site. As a minor if you wish to purchase or sell an item on the Site your legal guardian or parents who have registered as users of the Site may make such purchase or sale on your behalf. We reserve the right to terminate your membership and refuse to provide you with access to the Site if it is brought to our notice or if it is discovered that you are under the age of 18 years.</p></li>
+                  <li><p>We grant you a limited license to access and make personal use of the Site and the Service. This license does not include any downloading or copying of account information for the benefit of another vendor or any other third party; caching, unauthorized hypertext links to the Site and the framing of any content available through the Site uploading, posting, or transmitting any content that you do not have a right to make available (such as the intellectual property of another party); uploading, posting, or transmitting any material that contains software viruses or any other computer code, files or programs designed to interrupt, destroy or limit the functionality of any computer software or hardware or telecommunications equipment; any action that imposes or may impose (in our sole discretion) an unreasonable or disproportionately large load on our infrastructure; or any use of data mining, robots, or similar data gathering and extraction tools. You may not bypass any measures used by us to prevent or restrict access to the Site. Any unauthorized use by you shall terminate the permission or license granted to you by us.</p></li>
+                  </ol>
+                </li>
+                <li>
+                  <b>User Account, Password, and Security </b>
+                  <p>You will be able to generate a password and an account upon completing the Website's registration process. You are responsible for maintaining the confidentiality of the password and account, and are fully responsible for all activities that occur under your password or account. You agree to (i) immediately notify akrigi of any unauthorized use of your password or account or any other breach of security, and (ii) ensure that you exit from your account at the end of each session. (iii) You shall also ensure that you access the website from an authorized source and connection. akrigi cannot and will not be liable for any loss or damage arising from your failure to comply with this Section.</p>
+                  <p>You shall not host, display, upload, modify, publish, transmit, update or share any information or share/list(s) any information or item that:(a) belongs to another person and to which You do not have any right to (b) is grossly harmful, harassing, blasphemous, defamatory, obscene, pornographic, pedophilic, libelous, invasive of another's privacy, hateful, or racially, ethnically objectionable, disparaging, relating or encouraging money laundering or gambling, or otherwise unlawful in any manner, whatsoever (c) harm minors in any way (d) infringes any patent, trademark, copyright or other proprietary rights (e) violates any law for the time being in force (f) deceives or misleads the addressee/ users about the origin of such messages or communicates any information which is grossly offensive or menacing in nature (g) impersonate another person (h) contains software viruses or any other computer code, files or programs designed to interrupt, destroy or limit the functionality of any computer resource (i) threatens the unity, integrity, defense, security or sovereignty of India, friendly relations with foreign states, or public order or causes incitement to the commission of any cognizable offence or prevents investigation of any offence or is insulting any other nation (j) shall not create liability for us or cause us to lose (in whole or in part) the services of our ISPs or other suppliers; We will only use your Information in accordance with this Agreement and/or our Privacy Policy.</p>
+                </li>
+                <li>
+                  <b>Facilities available for Users</b>
+                  <p>akrigi provides a number of Internet-based facilities/services through its Website, with the core function of the Website being an online retail platform for pre-owned fashion apparels and accessories.<br/>Buyers/Sellers please note the following;</p>
+                  <ol type='a'>
+                    <li><p>Colors of the Products. While we have made every effort to display as accurately as possible the colors of the products that appear on the Site, we cannot guarantee that your monitor or screen’s display of any color will be completely accurate, as computer monitors and screens of electronic devices vary.</p></li>
+                    <li><p><h6>Electronic Communication</h6>When You use the Site or send emails or other data, information or communication to us, You agree and understand that You are communicating with us through electronic records and You consent to receive communications via electronic records from us periodically and as and when required. We will communicate with you by email or by an electronic record on our Site which will be deemed adequate service of notice / electronic record.</p></li>
+                  </ol>
+                </li>
+                <li>
+                  <b>Buyers of Akrigi</b>
+                  <p>The Buyers have the option to register on our website and create their account by generating their passwords. By using this option they would have the advantage to receive various information from us about our products according to their buying preferences. The Buyer can purchase ournew/second hand/pre-owned/pre-loved merchandise such as clothing, and accessories of various luxury and high street brands (collectively called, “Products"and/or “Items”). The Products can be purchased through our Website. We offer various methods of payments by the Buyers which includes Pre-payment before delivery by using Internet banking, Credit /Debit card & wallets facilities linked through our Payment Gateway partners.The sale/purchase of Products shall be additionally governed by specific policies such as cancellation, return& refund policy, privacy policy etc. (which are found on the URL http://www.akrigi.com), and all of which are incorporated here by reference. In addition, these terms and policies may be further supplemented by other material or information which may be displayed on the webpage.</p>
+                  <ol type='a'>
+                    <li>The Buying Process</li>
+                    <li>Pricing of our products.</li>
+                    <li> Cancellation by Us</li>
+                    <li>Cancellations by the User</li>
+                    <li>Fraudulent / Declined Transactions</li>
+                    <li>Credit /Debit Card Details</li>
+                    <li>Indemnity</li>
+                    <li>Invoicing and Taxation</li>
+                    <li>Delivery</li><br/>
+                  </ol>
+                </li>
+                <li>
+                  <b>Returns and refunds</b>
+                  <p>CLick <Link to='/returnpolicy'>here</Link> to Return and Refund policy</p>
+                </li>
+                <li>
+                  <b>Privacy Policy </b>
+                  <p>The user hereby consents, expresses and agrees that she/he has read and fully understands the Privacy Policy of akrigi.com. The user further consents that the terms and contents of such Privacy Policy are acceptable to him/her/them.</p>
+                </li>
+                <li>
+                  <b>User Warranty and Representation</b>
+                  <p>The user guarantees, warrants, and certifies that you are the owner of the content which you submit and are authorized to use the content and that the content does not infringe upon the property rights, intellectual property rights or other rights of others. You further warrant that to your knowledge, no action, suit, proceeding, or investigation has been instituted or threatened relating to any content, including trademark, trade name, service mark, and copyright formerly or currently used by you in connection with the services rendered by akrigi.</p>
+                </li>
+                <li>
+                  <b>Intellectual Property Rights</b>
+                  <p>Unless otherwise indicated or anything contained to the contrary or any proprietary material owned by a third party and so expressly mentioned, akrigi.com owns all Intellectual Property Rights to and into the Website, including, without limitation, any and all rights, title and interest in and to copyright, related rights, patents, utility models, trademarks, trade names, service marks, designs, know-how, trade secrets and inventions (whether patentable or not), goodwill, source code, meta tags, databases, text, content, graphics, icons, and hyperlinks. You acknowledge and agree that you shall not use, reproduce or distribute any content from the Website belonging to akrigi.  </p>
+                </li>
+                <li>
+                  <b> Links To Third Party Sites</b>
+                  <p>Due to social media integration, the Website may contain links to other websites ("Linked Sites"). The Linked Sites are not under the control of akrigi, and we are not responsible for the contents of any Linked Site, including without limitation any link contained in a Linked Site, or any changes or updates to a Linked Site. akrigi is not responsible for any form of transmission, whatsoever, received by you from any Linked Site. akrigi would only be providing these links to you only as a convenience, and the inclusion of any link does not imply endorsement by us or the Website of the Linked Sites or any association with its operators or owners including the legal heirs or assigns thereof. The users are requested to verify the accuracy of all information on their own before undertaking any reliance on such information.</p>
+                </li>
+              </ol>
+            </div>
+        </Row>
+      </Container>
+    </div>
+    
+  )
+}
+
+export default TC
